b6287039703b48c60c428f962986331102eee935
Viktor080181 <lozhnvikt0801@mail.ru>
2025-08-30 21:37:58 +0300

    State

--- src/component/App.js
+++ src/component/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './App.css';
 import Header from './header/Header.js';
 import Hello from './hello/Hello.js';
+import Length from './length/Length.js';
+
 class App extends React.Component {
 render() {
   return (
     <div className="App">
       <Header />
       <Hello />
-
+      <Length />
      
     </div>
   );
